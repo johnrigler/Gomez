eded0771a99fee2661f14ec9e8cc6049bbf7a91f
John Rigler <john@rigler.org>
2024-08-20 14:44:45 -0500

    Update gomez.sol
    Added comments

--- gomez.sol
+++ gomez.sol
@@ -6,6 +6,7 @@ contract globalTalk  {
 
 /* Copyright Secret Beach Solutions 2022 */
 /* John Rigler john.rigler@protonmail.com */
+/* john@rigler.org https://linkedin.com/jrigler */
 
 address public owner;
 
@@ -16,12 +17,20 @@ constructor() {
 function tell(
 
 /*
-This function does all of the heavy lifting. '86' is slang for cancel the order,
-so if you send an 86 to the address field, nothing address will be included. I give you
-potentially eight of these fields, so you could craft and send quite elaborate index structures
-across the currency. You need to input valid HTML, but that can be created beforehand. You
-can also just send text.
+The first variable ( a string called message ) is just that. It accepts any sort 
+of arbitrary data, provided that it is encoded into hex. This is the payload and
+is not read back from a smart contract. Instead, you can simply use a block explorer
+such as:
 
+https://api.polygonscan.com/api?module=account&action=txlist&address=0x5a2220d56f56db9c9f5b0cb83ff35b42746503a2&sort=desc
+
+You will need to read the data from the input field and convert it from hex to ascii (or whatever
+other format).  You will also need to skip the receiver# fields.
+
+The other four variables create an on-chain index. I usually only send 1 tiny unit of currency. While you 
+could send to actual addresses and even use this as a four-person airdrop, the idea here is that 
+you are encoding words into fake (unspendable) addresses that can be searched with the included
+javascript tool.
 */
 
     string memory message,
@@ -40,6 +49,13 @@ can also just send text.
     payable(receiver3).transfer(amount3);
     payable(receiver4).transfer(amount4);
     }
+
+/*
+This is just boilerplate stuff, the contract could acculumate crypto and has a way to send 
+it all to the contract creator. Generally, I don't do this because it seems like someone
+else could simply create their own contract and take a lower fee. So this is monetized
+offchain and is simply a free tool.
+*/
 
 function cashout ( uint256 amount ) public
     {
